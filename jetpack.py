439006d88de0901fe5d2a3a248f41753afef943c
noglitchjustswitch <luiriam.rosales+github@gmail.com>
2025-07-15 15:37:48 -0500

    Added instruction for path format on assets location.

--- jetpack.py
+++ jetpack.py
@@ -9,7 +9,7 @@ WIDTH = 1000
 HEIGHT = 600
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
-icon = pygame.image.load('assets\DialogueBarry0.png')
+icon = pygame.image.load('assets\DialogueBarry0.png') # if using linux based os, then the slash has to be backslash
 pygame.display.set_icon(icon)
 pygame.display.set_caption('Jetpack Joyride')
 fps = 60
